90005c1922d9e63d06e8d74a59d37d2c6094e6d5
chicheng567 <samuel910617@yahoo.com.tw>
2025-11-15 08:16:38 +0000

    Add flash attention

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -3,7 +3,8 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from transformer.Modules import ScaledDotProductAttention
-from flash_attn import flash_attn_varlen_qkvpacked_func
+import torch
+from flash_attn import flash_attn_varlen_qkvpacked_func, flash_attn_varlen_func
 __author__ = "Yu-Hsiang Huang"
 
 class MultiHeadAttention(nn.Module):
@@ -58,7 +59,83 @@ class MultiHeadAttention(nn.Module):
 
         return q, attn
 
+class MultiHeadSelfAttention_Flash(nn.Module):
+    ''' Multi-Head self Attention module with Flash Attention '''
 
+    def __init__(self, n_head, d_model, d_qkv, dropout=0.1, causal=False):
+        super().__init__()
+        self.n_head = n_head
+        self.d_qkv = d_qkv
+        self.w_qkv = nn.Linear(d_model, n_head * d_qkv * 3, bias=False)
+        self.w_o = nn.Linear(n_head * d_qkv, d_model, bias=False)
+        self.dropout_rate = dropout
+        self.dropout_layer = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        self.causal = causal
+    def forward(self, x, seq_lens):
+        # x should be of shape (batch_size * seq_len, d_model)
+        # seq_lens should be of shape (batch_size,) like (4, 3, 2, 23, 1, ...)
+        drop_rate = self.dropout_rate if self.training else 0.0
+        residual = x
+        qkv_packed = self.w_qkv(x).view(-1, 3, self.n_head, self.d_qkv)
+        cu_seqlens_B = torch.cumsum(seq_lens, dim=0, dtype=torch.int32)
+        cu_seqlens = F.pad(cu_seqlens_B, (1, 0), value=0)
+        max_len = seq_lens.max().item()
+        # output shape: (total_tokens, n_head, d_qkv)
+        output = flash_attn_varlen_qkvpacked_func(qkv_packed, cu_seqlens, max_len, dropout_p=drop_rate, causal=self.causal)
+        output = output.reshape(-1, self.n_head * self.d_qkv)
+        output = self.dropout_layer(self.w_o(output))
+        output += residual
+        output = self.layer_norm(output)
+        return output
+
+class MultiHeadCrossAttention_Flash(nn.Module):
+    def __init__(self, n_head, d_model, d_q, d_kv, dropout=0.1, causal=False):
+        super().__init__()
+        self.n_head = n_head
+        self.d_q = d_q
+        self.d_kv = d_kv
+        self.w_q = nn.Linear(d_model, n_head * d_q, bias=False)
+        self.w_kv = nn.Linear(d_model, n_head * d_kv * 2, bias=False)
+        self.w_o = nn.Linear(n_head * d_kv, d_model, bias=False)
+        self.dropout_layer = nn.Dropout(dropout)
+        self.dropout_rate = dropout
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        self.causal = causal
+    def forward(self, x_q, x_kv, seq_lens_q, seq_lens_kv):
+        # x_q should be of shape (batch_size * seq_len_q, d_model)
+        # x_kv should be of shape (batch_size * seq_len_kv, d_model)
+        # seq_lens_q should be of shape (batch_size,) like (4, 3, 2, 23, 1, ...)
+        # seq_lens_kv should be of shape (batch_size,) like (4, 3, 2, 23, 1, ...)
+        drop_rate = self.dropout_rate if self.training else 0.0
+        residual = x_q
+        q = self.w_q(x_q).view(-1, self.n_head, self.d_q)
+        kv = self.w_kv(x_kv).view(-1, 2, self.n_head, self.d_kv)
+        k = kv[:,0,:,:]
+        v = kv[:,1,:,:]
+        cu_seqlens_B_q = torch.cumsum(seq_lens_q, dim=0, dtype=torch.int32)
+        cu_seqlens_B_kv = torch.cumsum(seq_lens_kv, dim=0, dtype=torch.int32)
+        cu_seqlens_q = F.pad(cu_seqlens_B_q, (1, 0), value=0)
+        cu_seqlens_kv = F.pad(cu_seqlens_B_kv, (1, 0), value=0)
+        max_len_q = seq_lens_q.max().item()
+        max_len_kv = seq_lens_kv.max().item()
+        # output shape: (total_tokens_q, n_head, d_kv)
+        output = flash_attn_varlen_func(
+            q,
+            k,
+            v,
+            cu_seqlens_q=cu_seqlens_q,
+            cu_seqlens_kv=cu_seqlens_kv,
+            max_seqlen_q=max_len_q,
+            max_seqlen_kv=max_len_kv,
+            dropout_p=drop_rate,
+            causal=self.causal,
+        )
+        output = output.reshape(-1, self.n_head * self.d_kv)
+        output = self.dropout_layer(self.w_o(output))
+        output += residual
+        output = self.layer_norm(output)
+        return output
 class PositionwiseFeedForward(nn.Module):
     ''' A two-feed-forward-layer module '''
 
